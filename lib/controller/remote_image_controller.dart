dafe82864839579c6d6889078004e1af48a8ba1d
snowmerak <snowmerak@outlook.com>
2022-04-25 01:11:44 +0900

    fix buffer overflow

--- lib/controller/remote_image_controller.dart
+++ lib/controller/remote_image_controller.dart
@@ -1,15 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:fodman/podman/remote_image.dart';
+import 'package:fodman/podman/remote_image.dart' as podman;
 import 'package:get/get.dart';
+import 'package:tuple/tuple.dart';
 
 class RemoteImageController extends GetxController {
-  List<RemoteImage> images = [];
+  List<podman.RemoteImage> images = [];
   String? errorTitle;
   String? errorMessage;
 
   Future<void> loadImages(String name) async {
-    searchImage(name).then((resp) {
+    podman.searchImage(name).then((resp) {
       images = resp;
       update();
     }).onError((error, stackTrace) {
@@ -17,5 +18,10 @@ class RemoteImageController extends GetxController {
       errorMessage = stackTrace.toString();
       update();
     });
+  }
+
+  Future<Tuple2<String, String>> pullImage(String name, String tag) async {
+    var result = await podman.pullImage(name, tag);
+    return result;
   }
 }
