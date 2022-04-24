9ceffe3a5c834e2d19a10316d5004958ab6885d7
snowmerak <snowmerak@outlook.com>
2022-04-24 21:11:38 +0900

    make detail

---
+++ lib/pages/local_image/local_image_detail.dart
@@ -0,0 +1,173 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:fodman/controller/local_image_controller.dart';
+import 'package:get/get.dart';
+
+const localImageDetailPage = '/local_image_detail';
+
+class LocalImageDetailPage extends StatelessWidget {
+  const LocalImageDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.find<LocalImageController>();
+    var image = controller.selectedImage!;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(image.names?.first.split(":").first ?? ""),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Table(
+                columnWidths: {
+                  0: FlexColumnWidth(1.0),
+                  1: FlexColumnWidth(4.0),
+                },
+                border: TableBorder(
+                  horizontalInside: BorderSide(
+                    color: Colors.black12,
+                    width: 1.0,
+                  ),
+                ),
+                children: [
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Names'),
+                      ),
+                      TableCell(child: Text(image.names?.join("\n") ?? "")),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('ID'),
+                      ),
+                      TableCell(
+                        child: Text(image.id ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Parent ID'),
+                      ),
+                      TableCell(
+                        child: Text(image.parentId ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Repo Digests'),
+                      ),
+                      TableCell(
+                        child: Text(image.repoDigests?.join("\n") ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('size'),
+                      ),
+                      TableCell(
+                        child: Text(image.size?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Shared Size'),
+                      ),
+                      TableCell(
+                        child: Text(image.sharedSize?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Virtual Size'),
+                      ),
+                      TableCell(
+                        child: Text(image.virtualSize?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Labels'),
+                      ),
+                      TableCell(
+                        child: Text(image.labels?.maintainer ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Containers'),
+                      ),
+                      TableCell(
+                        child: Text(image.containers?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Digest'),
+                      ),
+                      TableCell(
+                        child: Text(image.digest!),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('History'),
+                      ),
+                      TableCell(
+                        child: Text(image.history?.join("\n") ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Created'),
+                      ),
+                      TableCell(
+                        child: Text(image.created?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Text('Created At'),
+                      ),
+                      TableCell(
+                        child: Text(image.createdAt?.toString() ?? ""),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
