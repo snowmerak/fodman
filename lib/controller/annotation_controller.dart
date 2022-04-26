c7b50dcc8c5f3d5e1634d36bf419518bec6e6a7d
snowmerak <snowmerak@outlook.com>
2022-04-26 17:28:15 +0900

    create annotion controller

---
+++ lib/controller/annotation_controller.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:get/get.dart';
+import 'package:tuple/tuple.dart';
+
+class AnnotationController extends GetxController {
+  List<Tuple2<String, String>> list = [Tuple2("", "")];
+
+  void append() {
+    list.add(Tuple2("", ""));
+    update();
+  }
+
+  void setAt(int index, String key, String value) {
+    list[index] = Tuple2(key, value);
+    update();
+  }
+
+  void removeAt(int index) {
+    list.removeAt(index);
+    update();
+  }
+}
