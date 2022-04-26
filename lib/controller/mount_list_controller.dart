eb55de769f21431643f155de90236c01ce7f6f59
snowmerak <snowmerak@outlook.com>
2022-04-27 00:06:24 +0900

    make mount list controller

---
+++ lib/controller/mount_list_controller.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:get/get.dart';
+import 'package:tuple/tuple.dart';
+
+class MountListController extends GetxController {
+  List<Tuple2<String, String>> volumes = [];
+  List<Tuple2<String, String>> binds = [];
+
+  void appendVolume() {
+    volumes.add(Tuple2("", ""));
+    update();
+  }
+
+  void appendBind() {
+    binds.add(Tuple2("", ""));
+    update();
+  }
+
+  void setVolume(int index, String host, String container) {
+    volumes[index] = Tuple2(host, container);
+    update();
+  }
+
+  void setBind(int index, String host, String container) {
+    binds[index] = Tuple2(host, container);
+    update();
+  }
+
+  void removeVolumeAt(int index) {
+    volumes.removeAt(index);
+    update();
+  }
+
+  void removeBindAt(int index) {
+    binds.removeAt(index);
+    update();
+  }
+}
