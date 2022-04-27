a14077964b734aa2350e09c06eb236393b52e095
snowmerak <snowmerak@outlook.com>
2022-04-27 19:13:56 +0900

    add run container

--- lib/controller/container_create_controller.dart
+++ lib/controller/container_create_controller.dart
@@ -1,4 +1,6 @@
+import 'package:fodman/podman/create_container.dart' as podman;
 import 'package:get/get.dart';
+import 'package:tuple/tuple.dart';
 
 class ContainerCreateController extends GetxController {
   String result = "";
@@ -6,5 +8,10 @@ class ContainerCreateController extends GetxController {
   void setResult(String value) {
     result = value;
     update();
+  }
+
+  Future<Tuple2<String, String>> runContainer(String arg) async {
+    var result = await podman.runContainer(arg);
+    return Tuple2(result.item1, result.item2);
   }
 }
