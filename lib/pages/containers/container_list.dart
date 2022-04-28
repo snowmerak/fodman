52903eaa2264e2667fc5fc1927a549528b7c867b
snowmerak <snowmerak@outlook.com>
2022-04-29 00:37:21 +0900

    make page file

---
+++ lib/pages/containers/container_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const containerListPage = "/containers/list";
+
+class ContainerListPage extends StatelessWidget {
+  const ContainerListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
