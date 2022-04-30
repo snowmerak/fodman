d79d1313305db667621520ab647a011c4b781972
snowmerak <snowmerak@outlook.com>
2022-04-30 14:32:15 +0900

    update UI to sliver and add moving to back when removed

--- lib/pages/local_image/local_image_detail.dart
+++ lib/pages/local_image/local_image_detail.dart
@@ -15,221 +15,234 @@ class LocalImageDetailPage extends StatelessWidget {
     var controller = Get.find<LocalImageController>();
     var image = controller.selectedImage!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(image.names?.first.split(":").first ?? ""),
-      ),
       body: Container(
         margin: EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Table(
-                columnWidths: {
-                  0: FlexColumnWidth(1.0),
-                  1: FlexColumnWidth(4.0),
-                },
-                border: TableBorder(
-                  horizontalInside: BorderSide(
-                    color: Colors.white12,
-                    width: 1.0,
-                  ),
-                ),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text(image.id ?? ""),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 12.0,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Row(
                 children: [
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Names'),
-                      ),
-                      TableCell(
-                        child: Column(
-                          children: (image.names ?? []).map((e) {
-                            return Row(
-                              children: [
-                                Text(e),
-                                SizedBox(
-                                  width: 16.0,
-                                ),
-                                TextButton(
-                                  // style: ButtonStyle(
-                                  //   backgroundColor: MaterialStateProperty.all(
-                                  //     Colors.lightBlue,
-                                  //   ),
-                                  // ),
-                                  onPressed: () {
-                                    controller.selectedTag = e;
-                                    Get.toNamed(createContainerPage);
-                                  },
-                                  child: Text(
-                                    "RUN",
-                                    style: TextStyle(
-                                      color: Colors.lightBlue,
-                                    ),
-                                  ),
-                                ),
-                                TextButton(
-                                  // style: ButtonStyle(
-                                  //   backgroundColor: MaterialStateProperty.all(
-                                  //     Colors.red,
-                                  //   ),
-                                  // ),
-                                  onPressed: () async {
-                                    var result = await controller.rmImage(e);
-                                    if (result.item1 != "") {
-                                      await showDialog(
-                                        context: Get.overlayContext!,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            title: Text("removed"),
-                                            content: Text(result.item1),
-                                          );
-                                        },
-                                      );
-                                      controller.loadImages();
-                                    } else {
-                                      await showDialog(
-                                        context: Get.overlayContext!,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content: Text(result.item2),
-                                          );
-                                        },
-                                      );
-                                    }
-                                  },
-                                  child: Text(
-                                    "REMOVE",
-                                    style: TextStyle(
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            );
-                          }).toList(),
+                  Expanded(
+                    child: Table(
+                      columnWidths: {
+                        0: FlexColumnWidth(1.0),
+                        1: FlexColumnWidth(4.0),
+                      },
+                      border: TableBorder(
+                        horizontalInside: BorderSide(
+                          color: Colors.white12,
+                          width: 1.0,
                         ),
                       ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('ID'),
-                      ),
-                      TableCell(
-                        child: Text(image.id ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Parent ID'),
-                      ),
-                      TableCell(
-                        child: Text(image.parentId ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Repo Digests'),
-                      ),
-                      TableCell(
-                        child: Text(image.repoDigests?.join("\n") ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('size'),
-                      ),
-                      TableCell(
-                        child: Text(image.size?.toString() ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Shared Size'),
-                      ),
-                      TableCell(
-                        child: Text(image.sharedSize?.toString() ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Virtual Size'),
-                      ),
-                      TableCell(
-                        child: Text(image.virtualSize?.toString() ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Labels'),
-                      ),
-                      TableCell(
-                        child: Text(image.labels?.maintainer ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Containers'),
-                      ),
-                      TableCell(
-                        child: Text(image.containers?.toString() ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Digest'),
-                      ),
-                      TableCell(
-                        child: Text(image.digest!),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('History'),
-                      ),
-                      TableCell(
-                        child: Text(image.history?.join("\n") ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Created'),
-                      ),
-                      TableCell(
-                        child: Text(image.created?.toString() ?? ""),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Text('Created At'),
-                      ),
-                      TableCell(
-                        child: Text(image.createdAt?.toString() ?? ""),
-                      ),
-                    ],
+                      children: [
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Names'),
+                            ),
+                            TableCell(
+                              child: Column(
+                                children: (image.names ?? []).map((e) {
+                                  return Row(
+                                    children: [
+                                      Text(e),
+                                      SizedBox(
+                                        width: 16.0,
+                                      ),
+                                      TextButton(
+                                        // style: ButtonStyle(
+                                        //   backgroundColor: MaterialStateProperty.all(
+                                        //     Colors.lightBlue,
+                                        //   ),
+                                        // ),
+                                        onPressed: () {
+                                          controller.selectedTag = e;
+                                          Get.toNamed(createContainerPage);
+                                        },
+                                        child: Text(
+                                          "RUN",
+                                          style: TextStyle(
+                                            color: Colors.lightBlue,
+                                          ),
+                                        ),
+                                      ),
+                                      TextButton(
+                                        // style: ButtonStyle(
+                                        //   backgroundColor: MaterialStateProperty.all(
+                                        //     Colors.red,
+                                        //   ),
+                                        // ),
+                                        onPressed: () async {
+                                          var result =
+                                              await controller.rmImage(e);
+                                          if (result.item1 != "") {
+                                            await showDialog(
+                                              context: Get.overlayContext!,
+                                              builder: (context) {
+                                                return AlertDialog(
+                                                  title: Text("removed"),
+                                                  content: Text(result.item1),
+                                                );
+                                              },
+                                            );
+                                            controller.loadImages();
+                                          } else {
+                                            await showDialog(
+                                              context: Get.overlayContext!,
+                                              builder: (context) {
+                                                return AlertDialog(
+                                                  title: Text("Error"),
+                                                  content: Text(result.item2),
+                                                );
+                                              },
+                                            );
+                                          }
+                                          Get.back();
+                                        },
+                                        child: Text(
+                                          "REMOVE",
+                                          style: TextStyle(
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('ID'),
+                            ),
+                            TableCell(
+                              child: Text(image.id ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Parent ID'),
+                            ),
+                            TableCell(
+                              child: Text(image.parentId ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Repo Digests'),
+                            ),
+                            TableCell(
+                              child: Text(image.repoDigests?.join("\n") ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('size'),
+                            ),
+                            TableCell(
+                              child: Text(image.size?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Shared Size'),
+                            ),
+                            TableCell(
+                              child: Text(image.sharedSize?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Virtual Size'),
+                            ),
+                            TableCell(
+                              child: Text(image.virtualSize?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Labels'),
+                            ),
+                            TableCell(
+                              child: Text(image.labels?.maintainer ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Containers'),
+                            ),
+                            TableCell(
+                              child: Text(image.containers?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Digest'),
+                            ),
+                            TableCell(
+                              child: Text(image.digest!),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('History'),
+                            ),
+                            TableCell(
+                              child: Text(image.history?.join("\n") ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Created'),
+                            ),
+                            TableCell(
+                              child: Text(image.created?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Text('Created At'),
+                            ),
+                            TableCell(
+                              child: Text(image.createdAt?.toString() ?? ""),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
