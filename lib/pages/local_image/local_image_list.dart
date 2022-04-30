24e4f3f78fa38c33109c054d7da37d23e26a5ad8
snowmerak <snowmerak@outlook.com>
2022-04-30 14:28:32 +0900

    update UI to sliver

--- lib/pages/local_image/local_image_list.dart
+++ lib/pages/local_image/local_image_list.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:fodman/controller/local_image_controller.dart';
 import 'package:fodman/pages/local_image/local_image_detail.dart';
@@ -13,26 +15,37 @@ class LocalImageListPage extends StatelessWidget {
     var controller = Get.put(LocalImageController());
     controller.loadImages();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image List'),
-      ),
-      body: GetBuilder<LocalImageController>(
-        builder: (imgs) => Center(
-          child: ListView.builder(
-            itemCount: imgs.images.length,
-            itemBuilder: (context, index) {
-              var key = imgs.imagesKeys[index];
-              var img = imgs.images[key]!;
-              return ListTile(
-                title: Text(key),
-                subtitle: Text(img.id!),
-                onTap: () {
-                  controller.selectedImage = img;
-                  Get.toNamed(localImageDetailPage);
-                },
-              );
-            },
-          ),
+      body: Container(
+        margin: EdgeInsets.all(12.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text('Local Images'),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 12.0,
+              ),
+            ),
+            GetBuilder<LocalImageController>(
+              builder: (controller) => SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    var img = controller.images[controller.imagesKeys[index]];
+                    return ListTile(
+                      title: Text(controller.imagesKeys[index]),
+                      subtitle: Text(img?.id ?? ""),
+                      onTap: () {
+                        controller.selectedImage = img;
+                        Get.toNamed(localImageDetailPage);
+                      },
+                    );
+                  },
+                  childCount: controller.imagesKeys.length,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
